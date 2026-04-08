bd504578ee9ac1e40a4dd494f6a7b28dabdf5afc
nikashitsa <vernik91@gmail.com>
2026-04-08 14:14:20 +0200

    feat: add stylelint-plugin-logical-css

--- index.js
+++ index.js
@@ -5,10 +5,20 @@
 const vueSyntax = require('postcss-html')();
 
 module.exports = {
+  plugins: ['stylelint-plugin-logical-css'],
   extends: [
     'stylelint-config-standard-scss',
     'stylelint-config-idiomatic-order',
   ],
+  rules: {
+    'logical-css/require-logical-keywords': true,
+    'at-rule-empty-line-before': null,
+    'declaration-empty-line-before': null,
+    'selector-class-pattern': [
+      '^[a-z0-9_-]*$',
+      {},
+    ],
+  },
   overrides: [
     {
       files: ['*.vue', '**/*.vue'],
